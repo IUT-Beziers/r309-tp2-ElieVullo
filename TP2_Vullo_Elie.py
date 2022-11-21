a90f46acbcee48505b4fe7f03c9329212fe7bfa3
Elie.Vullo <97040323+ElieVullo@users.noreply.github.com>
2022-11-21 18:05:18 +0100

    Add files via upload

--- TP2_Vullo_Elie.py
+++ TP2_Vullo_Elie.py
@@ -1,7 +1,16 @@
 from tkinter import *
 from tkinter import ttk
+import tkinter.font as font
+import random
 
-#Exercice 1 : 
+#Exercice 1 :
+
+
+def move(position):
+	global SwitchImage
+	Objet1 = can1.create_image(position.x,position.y, image=SwitchImage)
+	
+def find_closest	
 
 def affiche():
 	
@@ -11,43 +20,48 @@ def affiche():
 	B5.pack(side=LEFT)
 	can.create_window(66, 400, window=B4)
 	can.create_window(66, 450, window=B5)
+		
 
 def Switch():
-	SwitchImage = PhotoImage(file='/home/e/elie.vullo/Téléchargements/SwitchImage.png')
-	Objet = can1.create_image(300, 300, image = SwitchImage)
-	can1.image = SwitchImage
+	Image1 = can1.create_image(random.randint(0,300),random.randint(0,300), image = Images[0])
+	can1.image = Images[0]
+
 	
 def Routeur():
-	RouteurImage = PhotoImage(file='/home/e/elie.vullo/Téléchargements/RouteurImage.png')
-	Objet1 = can1.create_image(300, 300, image = RouteurImage)
-	can1.image = RouteurImage
+	Image2 = can1.create_image(random.randint(0,300),random.randint(0,300), image = Images[1])
+	can1.image = Images[1]
+	
 	
 def PC():
-	PcImage = PhotoImage(file='/home/e/elie.vullo/Téléchargements/PcImage.png')
-	Objet2 = can1.create_image(300, 300, image = PcImage)
-	can1.image = PcImage
+	Image3 = can1.create_image(random.randint(0,300),random.randint(0,300), image = Images[2])
+	can1.image = Images[2]
 	
 def Telephone():
-	TelephoneImage = PhotoImage(file='/home/e/elie.vullo/Téléchargements/TelephoneImage.png')
-	Objet3 = can1.create_image(300, 300, image = TelephoneImage)
-	can1.image = TelephoneImage
+	Image4 = can1.create_image(random.randint(0,300),random.randint(0,300), image = Images[3])
+	can1.image = Images[3]
 	
-	
-	
+
 root = Tk()
 root.title('Application réseau')
 root.geometry('720x720')
+
+SwitchImage = PhotoImage(file='/home/e/elie.vullo/Téléchargements/SwitchImage.png')
+RouteurImage = PhotoImage(file='/home/e/elie.vullo/Téléchargements/RouteurImage.png')
+PcImage = PhotoImage(file='/home/e/elie.vullo/Téléchargements/PcImage.png')
+TelephoneImage = PhotoImage(file='/home/e/elie.vullo/Téléchargements/TelephoneImage.png')
+Images = [SwitchImage, RouteurImage, PcImage, TelephoneImage]
+
+font = font.Font(size=15, weight="bold")
 
 can = Canvas(root, width=150, height=720, bg='ivory')
 can.pack(side=LEFT)
 can1 = Canvas(root, width=570, height=720, bg='white')
 can1.pack(side=RIGHT)
-can1.bind('<S>', Switch)
-can1.bind('<s>', Switch)
-can1.bind('<R>', Routeur)
-can1.bind('<r>', Routeur)
+can1.bind("<B1-Motion>", move)
 
-Label1 = Label(can, text='Menu', width=150, height=7, bg='red')
+
+Label1 = Label(can, text='Menu', width=300, height=7,borderwidth=2, bg='red')
+Label1['font'] = font
 Label1.pack()
 
 
