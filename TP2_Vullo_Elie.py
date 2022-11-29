14e18acb1eb765fd0d99e6a8c537e155e431d1c2
Elie.Vullo <97040323+ElieVullo@users.noreply.github.com>
2022-11-29 12:03:26 +0100

    Add files via upload

--- TP2_Vullo_Elie.py
+++ TP2_Vullo_Elie.py
@@ -5,12 +5,13 @@ import random
 
 #Exercice 1 :
 
+def move(a):
+	x = a.x
+	y = a.y
+	item = can1.find_closest(a.x, a.y)[0]
+	if item >= 0:
+		item = can1
 
-def move(position):
-	global SwitchImage
-	Objet1 = can1.create_image(position.x,position.y, image=SwitchImage)
-	
-def find_closest	
 
 def affiche():
 	
@@ -21,13 +22,26 @@ def affiche():
 	can.create_window(66, 400, window=B4)
 	can.create_window(66, 450, window=B5)
 		
+def suppr(position):
+	x = position.x
+	y = position.y
+	item = can1.find_closest(position.x, position.y)[0]
+	if item >= 0:
+		can1.delete(root,item)
 
 def Switch():
 	Image1 = can1.create_image(random.randint(0,300),random.randint(0,300), image = Images[0])
 	can1.image = Images[0]
 
-	
+def Switch2(S):
+	Image1 = can1.create_image(random.randint(0,300),random.randint(0,300), image = Images[0])
+	can1.image = Images[0]
+
 def Routeur():
+	Image2 = can1.create_image(random.randint(0,300),random.randint(0,300), image = Images[1])
+	can1.image = Images[1]
+	
+def Routeur2(R):
 	Image2 = can1.create_image(random.randint(0,300),random.randint(0,300), image = Images[1])
 	can1.image = Images[1]
 	
@@ -36,14 +50,27 @@ def PC():
 	Image3 = can1.create_image(random.randint(0,300),random.randint(0,300), image = Images[2])
 	can1.image = Images[2]
 	
+def PC2(P):
+	Image3 = can1.create_image(random.randint(0,300),random.randint(0,300), image = Images[2])
+	can1.image = Images[2]
+	
 def Telephone():
 	Image4 = can1.create_image(random.randint(0,300),random.randint(0,300), image = Images[3])
 	can1.image = Images[3]
 	
-
+def Telephone2(T):
+	Image4 = can1.create_image(random.randint(0,300),random.randint(0,300), image = Images[3])
+	can1.image = Images[3]
+	
 root = Tk()
 root.title('Application réseau')
 root.geometry('720x720')
+root.bind("<S>", Switch2)
+root.bind("<R>", Routeur2)
+root.bind("<P>", PC2)
+root.bind("<T>", Telephone2)
+
+
 
 SwitchImage = PhotoImage(file='/home/e/elie.vullo/Téléchargements/SwitchImage.png')
 RouteurImage = PhotoImage(file='/home/e/elie.vullo/Téléchargements/RouteurImage.png')
@@ -56,14 +83,13 @@ font = font.Font(size=15, weight="bold")
 can = Canvas(root, width=150, height=720, bg='ivory')
 can.pack(side=LEFT)
 can1 = Canvas(root, width=570, height=720, bg='white')
-can1.pack(side=RIGHT)
+can1.bind("<Double-Button-1>", suppr)
 can1.bind("<B1-Motion>", move)
-
+can1.pack(side=RIGHT)
 
 Label1 = Label(can, text='Menu', width=300, height=7,borderwidth=2, bg='red')
 Label1['font'] = font
 Label1.pack()
-
 
 
 B1 = Button(can, text='Switch', height=2, width=18, bg='gray', command = Switch)
